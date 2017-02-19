bb82e1df60d3c8884f2806c869a1617a67646387
iosci <iosci@myself.local>
2017-02-19 17:26:07 +0800

    add `WYPhotoBrowserFlutterTransition`, delete `WYPhotoBrowserAnimator`

---
+++ WYPhotoBrowser/WYPhotoBrowser/Animation/WYPhotoBrowserFlutterTransition.h
@@ -0,0 +1,13 @@
+//
+//  WYPhotoBrowserFlutterTransition.h
+//  WYPhotoBrowser
+//
+//  Created by iosci on 2017/2/19.
+//  Copyright © 2017年 secoo. All rights reserved.
+//
+
+#import "WYPhotoBrowserTransition.h"
+
+@interface WYPhotoBrowserFlutterTransition : WYPhotoBrowserTransition
+
+@end
---
+++ WYPhotoBrowser/WYPhotoBrowser/Animation/WYPhotoBrowserFlutterTransition.m
@@ -0,0 +1,13 @@
+//
+//  WYPhotoBrowserFlutterTransition.m
+//  WYPhotoBrowser
+//
+//  Created by iosci on 2017/2/19.
+//  Copyright © 2017年 secoo. All rights reserved.
+//
+
+#import "WYPhotoBrowserFlutterTransition.h"
+
+@implementation WYPhotoBrowserFlutterTransition
+
+@end
